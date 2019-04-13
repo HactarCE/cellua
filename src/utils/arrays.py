f82e31823f68d821cff2eef0e62159cd3025c408
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-04-13 00:44:51 -0400

    Forgot to add the updates utils.py

--- src/utils/arrays.py
+++ src/utils/arrays.py
@@ -9,9 +9,27 @@ def nd_cartesian_grid(*arrays, repeat=1):
     """
     return np.stack(np.meshgrid(*arrays * repeat, indexing='ij'), -1)
 
+
 def nd_cartesian(*arrays, repeat=1):
     """Generate a Cartesian product of ndarrays.
 
     The order of the returned values is the same as `itertools.product`.
     """
     return nd_cartesian_grid(*arrays, repeat=repeat).reshape(-1, len(arrays))
+
+
+def convert_to_coords(coords, d=None):
+    """Convert `coords` to a 1D integer ndarray of length `d`, or raise a
+    ValueError if it cannot be converted.
+    """
+    try:
+        coords = np.array(coords, dtype=np.int64)
+        if d is not None:
+            assert coords.shape == (d,)
+        return coords
+    except Exception:
+        pass
+    msg = f"Argument {coords} is not convertible to coordinate ndarray"
+    if d is not None:
+        msg += f" of shape ({d},)"
+    raise ValueError(msg)
