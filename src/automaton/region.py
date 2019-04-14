Tested, fixed, and extended region masking

src/automaton/region.py
@@ -1,17 +1,15 @@
 import numpy as np
-import itertools
 
 import utils
 
-
-# TODO: Test empty region. (creation and handling in all methods)
 
 class Region:
     """An immutable object describing an integer hyperrectangle.
 
     Public read-only properties:
     - empty -- bool; whether the region is empty; if this is True, then no other
-      public properties besides `dimensions`, `size`, and `shape` are defined.
+      public properties besides `dimensions`, `size`, `shape`, `has_mask`, and
+      `mask` are defined.
     - dimensions -- integer number of dimensions
     - bounds -- integer ndarray of shape (2, d); each row is a set of
       coordinate offsets from the center cell; for example [[-3, 0, -1], [2, 0,
@@ -78,14 +76,19 @@ class Region:
         the bounds will be shrunk accordingly.
         """
         bounds = np.array(bounds)  # Copy `bounds` and ensure it's an ndarray.
+        if bounds.shape and (bounds.ndim != 2 or bounds.shape[0] != 2):
+            return ValueError(f"Invalid region array shape: {bounds}")
         if not bounds.shape or (mask is not None and not mask.any()):
-            self.dimensions = len(bounds.shape) or int(bounds)
+            if bounds.shape:
+                self.dimensions = bounds.shape[1]
+            else:
+                self.dimensions = int(bounds)
             self.empty = True
             self.shape = (0,) * self.dimensions
             self.size = 0
+            self.has_mask = False
+            self.mask = None
             return
-        if bounds.ndim != 2 or bounds.shape[0] != 2:
-            return ValueError(f"Invalid region array shape: {bounds}")
         bounds.sort(0)  # Ensure that `lower_bounds < upper_bounds`.
         self.dimensions = bounds.shape[1]
         if isinstance(mask, np.ndarray):
@@ -175,7 +178,9 @@ class Region:
 
     def __repr__(self):
         s = f'{self.__class__.__name__}('
-        if not self.empty:
+        if self.empty:
+            s += f'{self.dimensions!r}'
+        else:
             s += f'{self.bounds!r}'
             if self.has_mask:
                 s += f', {self.mask!r}'
@@ -184,50 +189,45 @@ class Region:
 
     def __str__(self):
         if self.empty:
-            s = f'empty {region.dimensions}D region'
+            s = f'empty {self.dimensions}D region'
         else:
             s = 'x'.join(map(str, self.shape))
+            if self.dimensions == 1:
+                s += '-length'
             s += f' region from {tuple(self.lower_bounds)} to {tuple(self.upper_bounds)}'
             if self.has_mask:
                 s += f' with {np.count_nonzero(self.mask)}-element mask'
         return f'[{s}]'
 
-    # TODO: Test this method.
     def __eq__(self, other):
         if not isinstance(other, Region):
             return False
-        if self.empty and other.empty:
-            return True
+        if self.empty or other.empty:
+            return self.empty == other.empty
         return (np.all(self.bounds == other.bounds)
                 and np.all(self.mask == other.mask))
 
-    # TODO: Test this method.
     def __and__(self, other):
         """Return the intersection of two regions."""
         return self._boolean_op('&', other)
 
-    # TODO: Test this method.
     def __xor__(self, other):
         """Return the difference between two regions."""
         return self._boolean_op('^', other)
 
-    # TODO: Test this method.
     def __or__(self, other):
         """Return the union of two regions."""
         return self._boolean_op('|', other)
 
-    # TODO: Test this method.
     def __add__(self, other):
         """Offset a region by a coordinate tuple."""
         return self._offset(other, +1)
 
-    # TODO: Test this method.
     def __sub__(self, other):
         """Offset a region by a negated coordinate tuple."""
         return self._offset(other, -1)
 
-    # TODO: Test this method.
-    def __neg__(self, other):
+    def __neg__(self):
         """Invert a region along all axes. (See `Region.invert()`.)"""
         return self.invert()
 
@@ -265,53 +265,70 @@ class Region:
         if self.dimensions != other.dimensions:
             raise ValueError(f"Dimension mismatch between {self} and {other}")
         if not (self.empty or other.empty):
-            lower_bounds = np.minimum(self.lower_bounds, other.lower_bounds)
-            upper_bounds = np.maximum(self.upper_bounds, other.upper_bounds)
+            if op == '&':
+                # Optimization: When computing intersection beytween two regions
+                # with non-intersecting bounding boxes, the result is empty.
+                # (Remove masks because `Region.intersects()` depends on this
+                # function for handling masked regions.)
+                if not self.remask().intersects(other.remask()):
+                    return self.empty_copy()
+                # Optimization: When computing intersection, only include the
+                # intersection between the regions (obviously).
+                lower_bounds = np.maximum(self.lower_bounds, other.lower_bounds)
+                upper_bounds = np.minimum(self.upper_bounds, other.upper_bounds)
+                # Optimization: When computing intersection between two regions
+                # without masks, the final region does not need a mask.
+                if not (self.has_mask or other.has_mask):
+                    return Region([lower_bounds, upper_bounds])
+            else:
+                # For anything besides intersectioni, get the lower and upper
+                # bounds of the union of both regions. (Compute the mask later.)
+                lower_bounds = np.minimum(self.lower_bounds, other.lower_bounds)
+                upper_bounds = np.maximum(self.upper_bounds, other.upper_bounds)
             r = Region([lower_bounds, upper_bounds])
             new_mask = np.zeros(r.shape, dtype=bool)
-            self_slices = self.intersecting_slices(other)
-            other_slices = other.intersecting_slices(self)
-            new_mask[self_slices] = self.mask if self.has_mask else True
-            other_mask = other.mask if other.has_mask else True
+            new_mask[r.slices(self)] = self.mask[self.slices(r)] if self.has_mask else True
+            sliced_other_mask = other.mask[other.slices(r)] if other.has_mask else True
         if op == '&':
-            if self.empty or other.empty:
+            if self.empty:
                 return self
-            new_mask[other_slices] &= other_mask
+            if other.empty:
+                return other
+            new_mask[r.slices(other)] &= sliced_other_mask
         elif op == '|':
             if self.empty:
                 return other
             if other.empty:
                 return self
-            new_mask[other_slices] |= other_mask
+            new_mask[r.slices(other)] |= sliced_other_mask
         elif op == '^':
             if self.empty:
                 return other
             if other.empty:
                 return self
-            new_mask[other_slices] ^= other_mask
+            new_mask[r.slices(other)] ^= sliced_other_mask
         elif op == '&~':
             if self.empty or other.empty:
                 return self
-            new_mask[other_slices] &= ~other_mask
+            new_mask[r.slices(other)] &= ~sliced_other_mask
         return r.remask(new_mask)
 
-    # TODO: Test this method.
     def intersects(self, other):
         """Return whether `self` and `other` intersect at all."""
         if self.empty or other.empty:
             return False
         # Check whether the bounding rectangles intersect.
-        if not ((self.lower_bounds <= other.upper_bounds) |
+        if not ((self.lower_bounds <= other.upper_bounds) &
                 (other.lower_bounds <= self.upper_bounds)).all():
             return False
-        # If the bounding rectangles fit and neither region has a mask, then
+        # If the bounding rectangles overlap and neither region has a mask, then
         # they must intersect.
         if not (self.has_mask or other.has_mask):
             return True
-        return (self & other).mask.any()
+        return not (self & other).empty
 
-    def intersecting_slices(self, other):
-        """Return an iterator of the Slices that can be used to get the part of
+    def slices(self, other):
+        """Return an iterator of the slices that can be used to get the part of
         `self`'s mask which is inside of the bounding rectangle of `other`.
 
         Raises ValueError if `self` and `other` do not intersect.
@@ -323,11 +340,16 @@ class Region:
         shared_lower, shared_upper = np.clip(self.bounds, *other.bounds) - self.lower_bounds
         return tuple(map(slice, shared_lower, shared_upper + 1))
 
+    def empty_copy(self):
+        """Return an empty region with the same dimension count as this one."""
+        return self.__class__(self.dimensions)
+
     def minify(self):
         """Return a minified copy of this region; reduce the bounding box to the
         minimum possible according to the mask.
 
-        This has no effect on unmasked regions.
+        This has no effect on unmasked regions or regions created without
+        minify=False.
         """
         if self.empty:
             return self
@@ -369,14 +391,16 @@ class Region:
         else:
             return np.ones(self.shape, dtype=bool)
 
-    # TODO: Test this method.
     def get_coordinates_list(self):
         """Return a 2D ndarray listing of cell coordinates.
 
         The shape of the resulting ndarray is `(region.size, d)`. This result
         has the same contents as the iterator.
         """
-        return np.nonzero(self.get_mask()).T + self.lower_bounds
+        offsets = np.transpose(np.nonzero(self.get_mask()))
+        if self.empty:
+            return offsets
+        return offsets + self.lower_bounds
 
     def get_coordinates_grid(self):
         """Return a (d+1)-dimensionial ndarray of cell coordinates.
@@ -386,6 +410,9 @@ class Region:
         `get_coordinates_list()`.
         """
         # For each axis, get the range along that axis.
-        axis_ranges = map(np.arange, self.lower_bounds, self.upper_bounds + 1)
+        if self.empty:
+            axis_ranges = (np.arange(0),) * self.dimensions
+        else:
+            axis_ranges = map(np.arange, self.lower_bounds, self.upper_bounds + 1)
         # Take a Cartesian product of those ranges to get the offsets.
         return utils.arrays.nd_cartesian_grid(*axis_ranges)

src/test/automaton/custom_strategies.py
@@ -7,7 +7,9 @@ from automaton.grid import Grid
 from automaton.region import Region
 
 
-byte_strategy = lambda: st.integers(-128, 127)
+def byte_strategy():
+    return st.integers(-128, 127)
+
 
 def np_int64_arrays(shape, *args, **kwargs):
     """Return a strategy for integer ndarrays with a given shape.
@@ -15,6 +17,7 @@ def np_int64_arrays(shape, *args, **kwargs):
     All extra arguments are given to st.integers().
     """
     return np_st.arrays(np.int64, shape, st.integers(*args, **kwargs))
+
 
 def dimensions_strategy(min_dim=1, max_dim=10):
     """Return a strategy for reasonable cellular automaton dimension
@@ -27,6 +30,7 @@ def dimensions_strategy(min_dim=1, max_dim=10):
     - max_dimi (default 10)
     """
     return st.integers(min_dim, max_dim)
+
 
 def cell_coords_strategy(dimen, max_val=50):
     """Return a strategy for cell coordinates of a given dimensionality.
@@ -41,6 +45,7 @@ def cell_coords_strategy(dimen, max_val=50):
     """
     return np_int64_arrays(dimen, -max_val, max_val)
 
+
 def cell_offset_strategy(dimen, max_val=4):
     """Return a strategy for cell offsets of a given dimensionality.
 
@@ -54,11 +59,19 @@ def cell_offset_strategy(dimen, max_val=4):
     """
     return np_int64_arrays(dimen, -max_val, max_val)
 
+
 def grid_strategy(dimen):
     """Return a strategy for Grids of a given dimensionality."""
     return st.builds(Grid, st.just(dimen))
 
-def region_strategy(dimen, max_extent=50, *, max_cell_count=1000, random_mask=True, **kwargs):
+
+def region_strategy(dimen,
+                    max_extent=50,
+                    *,
+                    allow_empty=True,
+                    allow_nonrectangular=True,
+                    max_cell_count=1000,
+                    **kwargs):
     """Return a strategy for Regions of a given dimensionality.
 
     By default, the Region will extend at most 50 in every direction and will
@@ -71,23 +84,28 @@ def region_strategy(dimen, max_extent=50, *, max_cell_count=1000, random_mask=Tr
     - max_extent (default 50)
 
     Optional keyword arguments:
+    - allow_empty (default True)
+    - allow_nonrectangular (default True)
     - max_cell_count (default 1000)
-    - random_mask (default True) -- whether to use a not-all-True mask
 
     Any other keyword arguments are treated as strategies passed to Region().
     """
+
     max_extent = min(max_extent, int(max_cell_count ** (1 / dimen) / 2))
     bounds_strategy = np_int64_arrays((2, dimen), -max_extent, max_extent)
+
     def add_mask(region):
-        if random_mask:
-            def assume_some(mask):
-                assume(mask.any())
+        if allow_nonrectangular:
+            def assume_nonempty(mask):
+                if not allow_empty:
+                    assume(mask.any())
                 return mask
             # This `tuple(map(int, ...))` nonsense is stupid and I don't know
             # why it's necessary. (maybe bug in Hypothesis?)
             region_shape = tuple(map(int, region.shape))
-            mask_strat = np_st.arrays(bool, region_shape).map(assume_some)
+            mask_strat = np_st.arrays(bool, region_shape).map(assume_nonempty)
             return st.builds(region.remask, mask_strat)
         else:
             return st.just(region)
+
     return st.builds(Region, bounds_strategy).flatmap(add_mask)

src/test/automaton/test_region.py
@@ -1,12 +1,14 @@
-from hypothesis import given
+from hypothesis import given, note
 import hypothesis.strategies as st
 import hypothesis.extra.numpy as np_st
 import numpy as np
 
 from .custom_strategies import (
     dimensions_strategy,
+    cell_coords_strategy,
     region_strategy,
 )
+from automaton.region import Region
 
 
 @given(
@@ -18,19 +20,118 @@ def test_region(region):
     assert len(region) == len(positions)
     # Test that there are no duplicate coordinates.
     assert len(region) == len(set(map(tuple, positions)))
+    # Test region.get_coordinates_list() against iter(region).
+    coordinates_list = region.get_coordinates_list()
+    assert coordinates_list.tolist() == np.array(list(region)).tolist()
     # Test region.get_coordinates_grid() against iter(region.remask()).
     coordinates_grid = region.get_coordinates_grid()
-    flatter_positions = coordinates_grid.reshape((-1, region.dimensions))
-    assert flatter_positions.tolist() == np.array(list(region.remask())).tolist()
-    # Test that all coordinates are within the region.
+    flatter_grid = coordinates_grid.reshape((-1, region.dimensions))
+    assert flatter_grid.tolist() == np.array(list(region.remask())).tolist()
+    # Test position-in-region containment.
     for position in region:
         assert position in region
     # Test that not everything is within the region.
-    positions[0][0] -= 1  # Test below lower bound on first axis.
-    assert positions[0] not in region
-    if len(positions) > 1:  # TODO try remove the conditional
-        positions[-1][-1] += 1  # Test above upper bound on last axis.
+    if region.empty:
+        assert (0,) * region.dimensions not in region
+    else:
+        # - Test below lower bound on first axis.
+        positions[0][0] -= 1
+        assert positions[0] not in region
+        positions[0][0] += 1
+        # - Test above upper bound on last axis.
+        positions[-1][-1] += 1
         assert positions[-1] not in region
+        positions[-1][-1] -= 1
+    # Test equality and minifying.
+    assert region == region.minify()
+    assert (region != region.remask()) == region.has_mask
+    # Test stringifying.
+    assert str(region)
+    assert repr(region)
+
+
+@given(
+    regions=dimensions_strategy(max_dim=4).flatmap(lambda d: st.tuples(
+        region_strategy(d),
+        region_strategy(d),
+    ))
+)
+def test_region_operators(regions):
+    r1, r2 = regions
+    note('r1 = ' + str(r1))
+    note('r2 = ' + str(r2))
+    should_intersect = any(pos in r2 for pos in r1)
+    assert should_intersect == r1.intersects(r2)
+    intersection = r1 & r2
+    difference = r1 ^ r2
+    union = r1 | r2
+    subtraction = r1 ^ r1 & r2
+    note('and = ' + str(intersection))
+    note('xor = ' + str(difference))
+    note('or  = ' + str(union))
+    note('sub = ' + str(subtraction))
+    for pos in r1:
+        assert pos in union
+        if pos in r2:
+            # Test cells in r1 and r2.
+            assert pos in intersection
+            assert pos not in difference
+            assert pos not in subtraction
+        else:
+            # Test cells in r1 but not r2.
+            assert pos not in intersection
+            assert pos in difference
+            assert pos in subtraction
+    for pos in r2:
+        if pos not in r1:
+            # Test cells in r2 but not r1.
+            assert pos not in intersection
+            assert pos in difference
+            assert pos in union
+            assert pos not in subtraction
+    for pos in intersection:
+        assert pos in r1 and pos in r2
+    for pos in union:
+        assert pos in r1 or pos in r2
+    for pos in difference:
+        assert (pos in r1) != (pos in r2)
+    for pos in subtraction:
+        assert pos in r1 and pos not in r2
+    # Test region-in-region containment.
+    assert intersection in r1 and intersection in r2
+    assert r1 in union and r2 in union
+    assert subtraction in r1
+    # Test other miscellaneous properties.
+    assert (r1 == intersection) == (r1 in r2)
+    assert (r2 == intersection) == (r2 in r1)
+    assert (r1 == difference) == r2.empty
+    assert (r2 == difference) == r1.empty
+    assert (r1 == union) == (r2 in r1)
+    assert (r2 == union) == (r1 in r2)
+    assert (r1 == subtraction) == intersection.empty
+
+
+@given(
+    dimensioned_args=dimensions_strategy().flatmap(lambda d: st.tuples(
+        region_strategy(d),
+        cell_coords_strategy(d),
+    ))
+)
+def test_region_offset(dimensioned_args):
+    region, offset = dimensioned_args
+    added = region + offset
+    subtracted = region - offset
+    # Test invariant properties, such as number of cells and emptiness.
+    assert len(added) == len(subtracted) == len(region)
+    assert added.empty == subtracted.empty == region.empty
+    if offset.any() and not region.empty:
+        assert added != subtracted != region
+    else:
+        assert added == subtracted == region
+    for pos in region:
+        assert pos + offset in added
+        assert pos - offset in subtracted
+
 
 @given(
     dimensioned_args=dimensions_strategy().flatmap(lambda d: st.tuples(
@@ -44,7 +145,9 @@ def test_region(region):
 )
 def test_region_invert(dimensioned_args):
     region, axes = dimensioned_args
-    # Test that the inverse of the inverse is the original
+    # Test "negation."
+    assert region.invert() == -region
+    # Test that the inverse of the inverse is the original.
     assert region.invert().invert() == region
     assert region.invert(axes).invert(axes) == region
     if not axes:
@@ -53,7 +156,7 @@ def test_region_invert(dimensioned_args):
 
 @given(
     unminified_region=dimensions_strategy().flatmap(lambda d:
-        region_strategy(d, minify=st.just(False)),
+        region_strategy(d, allow_empty=False, minify=st.just(False)),
     )
 )
 def test_region_minify(unminified_region):
@@ -66,9 +169,40 @@ def test_region_minify(unminified_region):
         assert rolled_minified[0].any()
         assert rolled_minified[-1].any()
     # And make sure that nothing got cut off.
-    slices = minified_region.intersecting_slices(unminified_region)
+    slices = minified_region.slices(unminified_region)
     mask_copy = minified_region.get_mask().copy()
     # Cut out the part that's in the minified Region ...
     mask_copy[slices] = False
     # ... and check that the rest is false.
     assert not mask_copy.any()
+
+
+@given(
+    original=dimensions_strategy().flatmap(region_strategy)
+)
+def test_empty_region(original):
+    # Test minifying to an empty region.
+    new_mask = original.get_mask().copy()
+    new_mask.fill(False)
+    region = original.remask(new_mask)
+    assert region.empty
+    # Test position-in-region containment.
+    for pos in original:
+        assert pos not in region
+    # Test region-in-region containment.
+    assert region in original
+    assert (original in region) == original.empty
+    # Test iteration.
+    assert len(list(region)) == 0
+    # Test length.
+    assert len(region) == 0
+    # Test stringifying.
+    assert str(region)
+    assert repr(region)
+    # Test dimension count and construction from dimension count.
+    region2 = region.empty_copy()
+    region3 = Region(region.dimensions)
+    region4 = region3.empty_copy()
+    # Test equality.
+    assert region == region2 == region3 == region4
+    assert (region == original) == original.empty